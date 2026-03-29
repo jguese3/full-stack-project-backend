0a14aac29fed395211bfa7d2efb429f1c806d48d
jguese3 <jguese3@rrc.ca>
2026-03-29 18:21:57 -0500

    merged develop

---
+++ src/api/v1/controllers/userController.ts
@@ -0,0 +1,11 @@
+import { NextFunction } from "express";
+
+export const getUserById = async(
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const user: 
+    }
+}
