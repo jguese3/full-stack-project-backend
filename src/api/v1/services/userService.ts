320b1abed4528f389c6f5d24238b9eb532589925
jguese3 <jguese3@rrc.ca>
2026-03-29 19:31:15 -0500

    added service for users

--- src/api/v1/services/userService.ts
+++ src/api/v1/services/userService.ts
@@ -1,2 +1,21 @@
 import { User } from "@prisma/client"
 import prisma from "../../../../prisma/client"
+
+export const getUserById = async(id: number): Promise<User | null> => {
+    try {
+        // get first record that match the "where" object key/value pairs
+        const user = prisma.user.findUnique({
+            where: {
+                id: id
+            }
+        });
+
+        if(!user) {
+            return null;
+        } else{
+            return user;
+        }
+    } catch(error) {
+        throw new Error(`Failed to fetch user with id ${id}`);
+    }
+}
