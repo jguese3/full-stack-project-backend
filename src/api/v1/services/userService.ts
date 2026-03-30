76274b05b7dc5f0201b62a1c22f81f094d10dd1d
jguese3 <jguese3@rrc.ca>
2026-03-29 19:37:19 -0500

    copied response model from the demo to use for the project

--- src/api/v1/services/userService.ts
+++ src/api/v1/services/userService.ts
@@ -3,7 +3,6 @@ import prisma from "../../../../prisma/client"
 
 export const getUserById = async(id: number): Promise<User | null> => {
     try {
-        // get first record that match the "where" object key/value pairs
         const user = prisma.user.findUnique({
             where: {
                 id: id
